db6d8ac3282829e4148e94406f554e2e3c6b3cd7
ren-pillai30 <aryanpillai8a@gmail.com>
2025-06-18 14:11:55 +0000

    Changed borders from round to sharp, changed font for info text

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,11 +3,11 @@
 import React from "react";
 
 type Props = {};
-
+//base characteristics of each event card
 const boxStyle = {
-  width: "200px",
-  height: "184px",
-  borderRadius: "8px",
+  width: "340px",
+  height: "308px",
+  borderRadius: "0px",
   display: "flex",
   flexDirection: "column" as const,
   alignItems: "flex-start",
@@ -23,62 +23,63 @@ const LandingPage = (props: Props) => {
       <h1 className="text-4xl font-press-start text-center mt-10 w-full max-w-screen-lg">
         EVENTS
       </h1>
-      <div className="flex flex-col gap-10 my-10">
+      <div className="flex flex-col gap-10 my-20">
         {/* First row of 3 boxes */}
-        <div className="flex flex-row gap-8 justify-start">
+        <div className="flex flex-row gap-10 justify-start">
+          {/*card charcateristics such as fill color,text color and border dimensions*/} 
           <div
-            className="text-1xl font-press-start"
+            className="text-2xl font-press-start"
             style={{ ...boxStyle, backgroundColor: "#FFDFE8", color: "#6d1c22",border:"12px solid #E8A2B5" }}>
             Event Name 
-            <p style={{ fontWeight: "normal", fontSize: "8px", marginTop: "10px" }}>
+            <p style={{ fontWeight: "normal", fontSize: "16px", marginTop: "10px", fontFamily: "'IBM Plex Mono', monospace" }}>
     This is the info text for the first event. Add more details here.
           </p>
           </div>
           <div
-            className="text-1xl font-press-start"
+            className="text-2xl font-press-start"
             style={{ ...boxStyle, backgroundColor: "#C5FFD8", color: "#095709", border:"12px solid #ABEEAB"}}
           >
             Event Name 
-            <p style={{ fontWeight: "normal", fontSize: "8px", marginTop: "10px" }}>
+            <p style={{ fontWeight: "normal", fontSize: "16px", marginTop: "10px", fontFamily: "'IBM Plex Mono', monospace"}}>
     This is the info text for the second event. Add more details here.
           </p>
           </div>
           <div
-            className="text-1xl font-press-start"
+            className="text-2xl font-press-start"
             style={{ ...boxStyle, backgroundColor: "#CBF1FD", color:"#0A3A6b", border:"12px solid #B3D9FF" }}
           >
             Event Name
-            <p style={{ fontWeight: "normal", fontSize: "8px", marginTop: "10px" }}>
+            <p style={{ fontWeight: "normal", fontSize: "16px", marginTop: "10px", fontFamily: "'IBM Plex Mono', monospace"}}>
     This is the info text for the third event. Add more details here.
           </p> 
           </div>
         </div>
         {/* Second row of 3 boxes */}
-        <div className="flex flex-row gap-8 justify-start">
+        <div className="flex flex-row gap-10 justify-start">
           <div
-            className="text-1xl font-press-start"
+            className="text-2xl font-press-start"
             style={{ ...boxStyle, backgroundColor: "#CBF1FD", color:"#0A3A6b", border:"12px solid #B3D9FF"}}
           >
             Event Name
-            <p style={{ fontWeight: "normal", fontSize: "8px", marginTop: "10px" }}>
+            <p style={{ fontWeight: "normal", fontSize: "16px", marginTop: "10px", fontFamily: "'IBM Plex Mono', monospace"}}>
     This is the info text for the fourth event. Add more details here.
           </p> 
           </div>
           <div
-            className="text-1xl font-press-start"
+            className="text-2xl font-press-start"
             style={{ ...boxStyle, backgroundColor: "#fff4dd", color: "#865B00", border:"12px solid #FFD782"}}
           >
             Event Name 
-            <p style={{ fontWeight: "normal", fontSize: "8px", marginTop: "10px" }}>
+            <p style={{ fontWeight: "normal", fontSize: "16px", marginTop: "10px", fontFamily: "'IBM Plex Mono', monospace"}}>
     This is the info text for the fifth event. Add more details here.
           </p> 
           </div>
           <div
-            className="text-1xl font-press-start"
+            className="text-2xl font-press-start"
             style={{ ...boxStyle, backgroundColor: "#ffdfe8" , color: "#6d1c22", border:"12px solid #E8A2B5"}}
           >
             Event Name 
-            <p style={{ fontWeight: "normal", fontSize: "8px", marginTop: "10px" }}>
+            <p style={{ fontWeight: "normal", fontSize: "16px", marginTop: "10px", fontFamily: "'IBM Plex Mono', monospace"}}>
     This is the info text for the sixth event. Add more details here.
           </p>
           </div>
