4cac511fa7baa062c4d0941eb202e8912897eb66
Sri Saidhakshini V <srisaidhakshiniv@gmail.com>
2026-06-29 12:54:14 +0530

    Update lead images

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'h8z6stjynz.ufs.sh',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.phototourl.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
